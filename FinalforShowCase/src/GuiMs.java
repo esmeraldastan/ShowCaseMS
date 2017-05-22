2751800589b017b452ded55105232e558892198e
Chase <esmeraldarekz99@gmail.com>
2017-05-22 14:24:44 -0700

    Cells?
    have been added

--- FinalforShowCase/src/GuiMs.java
+++ FinalforShowCase/src/GuiMs.java
@@ -2,8 +2,12 @@
 //import 
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 
-public class GuiMs {
+public class GuiMs implements ActionListener{
 	//Top section 
 	JFrame frame = new JFrame("Welcome to Minesweeper");
 	//Button to restart the game 
@@ -11,9 +15,28 @@ public class GuiMs {
 	//10x10
 	JButton[][] button = new JButton[10][10];
 	int[][] numbs = new int[10][10];
+	Container grid = new Container();
+	
 	
 	public GuiMs(){
-		frame.setSize(500,500);
+		frame.setSize(700,700);
+		//reset.setSize(10,10);
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(reset,BorderLayout.SOUTH);
+		reset.addActionListener(this);
+		grid.setLayout(new GridLayout(10,10));
+		//frame.setSize(reset, 10, 10);
+		for(int row = 0; row < button.length; row++){
+			for( int column = 0; column < button.length; column++){
+				button[row][column] = new JButton();
+				button[row][column].addActionListener(this);
+				grid.add(button[row][column]);
+				
+				
+			}
+		}
+		frame.add(grid, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
