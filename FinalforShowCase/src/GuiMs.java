72fd73d9bde7457213085704d9ba783391f37641
Chase <esmeraldarekz99@gmail.com>
2017-05-22 13:14:38 -0700

    Setting up
    Starting to set up the grid

--- FinalforShowCase/src/GuiMs.java
+++ FinalforShowCase/src/GuiMs.java
@@ -1,10 +1,18 @@
 //this code will be use instead
+//import 
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
 public class GuiMs {
-	
-	//JFrame frame = new JFrame("Welcome to Minesweeper");
-	
-	public static void main(String[] args){
-	System.out.println("Hello");
+	//Top section 
+	JFrame frame = new JFrame("Welcome to Minesweeper");
+	//Button to restart the game 
+	JButton reset = new JButton("Want to reset??"); 
+	//10x10
+	JButton[][] button = new JButton[10][10];
+	int[][] numbs = new int[10][10];
+	//public static void main(String[] args){
+	//System.out.println("Hello");
 
 }
-}
+
