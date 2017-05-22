9d3ee9f79fdd5e5a75c93dfb480efe501bfa914a
Chase <esmeraldarekz99@gmail.com>
2017-05-22 13:06:00 -0700

    Gui Class

--- FinalforShowCase/src/GuiMs.java
+++ FinalforShowCase/src/GuiMs.java
@@ -1,5 +1,8 @@
 //this code will be use instead
 public class GuiMs {
+	
+	//JFrame frame = new JFrame("Welcome to Minesweeper");
+	
 	public static void main(String[] args){
 	System.out.println("Hello");
 
