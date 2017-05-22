1b0d64e516d44e26cdc341edf45a074aa9977ebc
Chase <esmeraldarekz99@gmail.com>
2017-05-22 00:32:07 -0700

    New Class`
    original code is being added

--- FinalforShowCase/src/GuiMs.java
+++ FinalforShowCase/src/GuiMs.java
@@ -1,4 +1,4 @@
-
+//this code will be use instead
 public class GuiMs {
 	public static void main(String[] args){
 	System.out.println("Hello");
---
+++ FinalforShowCase/src/Minesweeper.java
@@ -0,0 +1,24 @@
+import java.lang.Math;
+
+public class Minesweeper {
+	
+	int [][] board;
+	int row;
+	int column;
+	int XPos;
+	int YPos;
+	
+	public Minesweeper(int rows, int colums){
+		this.row = rows;
+		this.column = colums;
+		board = new int [row[[column]
+	}
+	public static void main(String[] args){
+		System.out.println("New Game");
+	}
+}
+
+	
+	
+
+
