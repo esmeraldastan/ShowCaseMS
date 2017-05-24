1cbdc48df0d245b95c335371a66a46562f3dca18
Chase <esmeraldarekz99@gmail.com>
2017-05-24 14:24:14 -0700

    save

--- FinalforShowCase/src/Minesweeper.java
+++ FinalforShowCase/src/Minesweeper.java
@@ -12,6 +12,30 @@ public class Minesweeper {
 		this.row = rows;
 		this.column = colums;
 		board = new int [row][column];
+		
+	}
+	public Minesweeper(){
+		this(10,10);
+		
+	}
+	
+	public void printBoard2(){
+		System.out.println("******* WELCOME TO MINESWEEPER*******");
+		System.out.println();
+		
+		for(int rows = 0; rows < row; rows++){
+			for( int columns = 0; columns < column; columns++){
+		if ( board[row][column] == -1){
+			System.out.print(" * ");
+		
+		}
+		else {
+			System.out.print(board[rows][columns] + " ");
+		}
+	}
+			System.out.println(" ");
+		}
+		
 	}
 	public static void main(String[] args){
 		System.out.println("New Game");
