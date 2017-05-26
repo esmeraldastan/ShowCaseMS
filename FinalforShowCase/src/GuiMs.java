2708742c4e6bffe55577068980c8900be0e98563
Chase <esmeraldarekz99@gmail.com>
2017-05-25 23:35:03 -0700

    save

--- FinalforShowCase/src/GuiMs.java
+++ FinalforShowCase/src/GuiMs.java
@@ -2,57 +2,64 @@ import java.lang.Math;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 
 //new code
 //Mr. Wiebe's Base Code
 
 public class GuiMs{
-	Cell[][] board;
-	int row;
-	int column;
-	public GuiMS(){
-		this(10,10,50);
-	}
-	public GuiMs(int rows, int columns, int bombs){
-		this.row = rows;
-		this.column = columns;
-		grid = new Cell[row][column];
-		
-		//frame set up
-		JFrame frame = new JFrame();
-		frame.add(addCells());
-		
-		try{
-			this.addBombs(bombs);
-		}catch (Exception e);
-			System.out.println("An Exception was thrown:" = e);
-	}
-	this.addNums();
-	this.printBoard();
-	
-	frame.pack();
+    Cell[][] board;
+    int rows;
+    int columns;
+    public GuiMs(){
+        this(10, 110, 150);
+    }
 
-	frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	frame.setVisible(true);
-	}
-	public void addBombs(int bombs)throw Excepion{
-		if bombs > rows* columns || bombs < 0{
-			throw new Exception("Can not set that number of bombs.");
-		}
-		while(bombs > 0){
-			int ranRow = (int)(Math.random()*rows);
-			int ranCol = (int)(Math.random()*colums);
-			if(board[ranRow][ranCol].getValue()==0){
-				board[ranRow][ranCol].changeValue(-1);
-				bombs = -1;
-			}
-		}
-	}
-	public void addNums(){
-		for( int r = 0; r < rows; r++){
-			for( int c = 0; c < columns; c++){
-				if (board[r][c].getValue() == -1){
+    public GuiMs(int rows, int columns, int bombs){
+        //Put the constructor here.
+        this.rows = rows;
+        this.columns = columns;
+
+        board = new Cell[rows][columns];
+       
+
+        //These pieces are for the GUI.
+        JFrame frame = new JFrame();
+        frame.add(addCells());
+
+         try{
+            this.addBombs(bombs);
+        }catch (Exception e){
+            System.out.println("An Exception was thrown: " + e);
+        }
+        this.addNums();
+        this.printBoard();
+        
+        frame.pack();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public void addBombs(int bombs) throws Exception{
+        if (bombs > rows*columns || bombs < 0){
+            throw new Exception("Invalid number of bombs");
+        }
+        while (bombs > 0){
+            int randRow = (int)(Math.random()*rows);
+            int randCol = (int)(Math.random()*columns);
+            if (board[randRow][randCol].getValue() == 0){
+                board[randRow][randCol].changeValue(-1);
+                bombs-=1;
+            }
+        }
+    }
+
+    public void addNums(){
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                if (board[r][c].getValue() == -1){
                     //Left side
                     if(c-1 >= 0 && board[r][c-1].isClear()){
                         board[r][c-1].incValue();
@@ -88,77 +95,90 @@ public class GuiMs{
                 }
             }
         }
-    }	
-	 public void scanForEmptyCells(){
-	        for(int i = 0; i<rows; i++){
-	            for(int j = 0; j<columns; j++){
-	                if(!board[i][j].isNotChecked()){
-	                    if(j>=1 && board[i][j-1].isEmpty()){
-	                    	board[i][j-1].checkCell();
-	                    }
-	                    if(j<columns && board[i][j+1].isEmpty()){
-	                    	board[i][j+1].checkCell();
-	                    }
-	                    if(i>=1 && board[i-1][j].isEmpty()){
-	                    	board[i-1][j].checkCell();
-	                    }
-	                    if(i< rows && board[i+1][j].isEmpty()){
-	                    	board[i+1][j].checkCell();
-	                    }
-	                    if(i>=1 && j>= 1 && board[i-1][j-1].isEmpty()){
-	                    	board[i-1][j-1].checkCell();
-	                    }
-	                    if(i+1< rows && j+1< columns && board[i+1][j+1].isEmpty()){
-	                    	board[i+1][j+1].checkCell();
-	                    }
-	                    if(i>=1 && j+1< columns && board[i-1][j+1].isEmpty()){
-	                    	board[i-1][j+1].checkCell();
-	                    }
-	                    if(i+1< rows && j>= 1 && board[i+1][j-1].isEmpty()){
-	                    	board[i+1][j-1].checkCell();
-	                    }
-	                }
-	            }
-	        }
-	    }
-	    /**This method is used for testing and will be deleted if using the GUI.
-	     *  It is still required for all students.
-	     */
-	    public void printBoard(){
-	        System.out.println();
-	        System.out.println("<<<<<<<<<<WELCOME TO MINESWEEPER>>>>>>>>>>");
-	        System.out.println();
-	        for(int row = 0; row < rows; row++){
-	            for(int col = 0; col < columns; col++) {
-	                if (board[row][col].getValue() == -1){
-	                    System.out.print("X ");
-	                }
-	                else{
-	                    System.out.print(board[row][col].getValue() + " ");
-	                }
-	            }
-	            System.out.println();
-	        }
-	    }
+    }
+    
+    public void scanForEmptyCells(){
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<columns; j++){
+                if(!board[i][j].isNotChecked()){
+                    if(j>=1 && board[i][j-1].isEmpty()){
+                    	board[i][j-1].checkCell();
+                    }
+                    if(j<columns && board[i][j+1].isEmpty()){
+                    	board[i][j+1].checkCell();
+                    }
+                    if(i>=1 && board[i-1][j].isEmpty()){
+                    	board[i-1][j].checkCell();
+                    }
+                    if(i< rows && board[i+1][j].isEmpty()){
+                    	board[i+1][j].checkCell();
+                    }
+                    if(i>=1 && j>= 1 && board[i-1][j-1].isEmpty()){
+                    	board[i-1][j-1].checkCell();
+                    }
+                    if(i+1< rows && j+1< columns && board[i+1][j+1].isEmpty()){
+                    	board[i+1][j+1].checkCell();
+                    }
+                    if(i>=1 && j+1< columns && board[i-1][j+1].isEmpty()){
+                    	board[i-1][j+1].checkCell();
+                    }
+                    if(i+1< rows && j>= 1 && board[i+1][j-1].isEmpty()){
+                    	board[i+1][j-1].checkCell();
+                    }
+                }
+            }
+        }
+    }
+    /**This method is used for testing and will be deleted if using the GUI.
+     *  It is still required for all students.
+     */
+    public void printBoard(){
+        System.out.println();
+        System.out.println("<<<<<<<<<<WELCOME TO MINESWEEPER>>>>>>>>>>");
+        System.out.println();
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < columns; col++) {
+                if (board[row][col].getValue() == -1){
+                    System.out.print("X ");
+                }
+                else{
+                    System.out.print(board[row][col].getValue() + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
-	    public JPanel addCells(){
-	        JPanel panel = new JPanel(new GridLayout(rows,columns));
-	        for(int i = 0; i < rows; i++){
-	            for(int j = 0; j < columns; j++){
-	                board[i][j]= new Cell(this);
-	                panel.add(board[i][j].getButton());
-	            }
-	        }
-	        return panel;
-	    }
-	    public void fail(){
-	        for(Cell[] a : board){
-	            for(Cell b : a){
-	                b.displayValue();
-	            }
-	        }
-	    }
-	    public static void main(String[] args){
-	    	}
+    public JPanel addCells(){
+        JPanel panel = new JPanel(new GridLayout(rows,columns));
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                board[i][j]= new Cell(this);
+                panel.add(board[i][j].getButton());
+            }
+        }
+        return panel;
+    }
+    public void fail(){
+        for(Cell[] a : board){
+            for(Cell b : a){
+                b.displayValue();
+            }
+        }
+    }
+    public static void main(String[] args){
+    	Scanner scanner = new Scanner(System.in);
+		System.out.print("How many rows do you want? ");
+	    int rows = scanner.nextInt();
+	    Scanner scanner2 = new Scanner(System.in);
+		System.out.print("How many columns do you want? ");
+		int columns = scanner2.nextInt();
+	    Scanner scanner3 = new Scanner(System.in);
+		System.out.print("How many bombs do you want? ");
+		int bombs = scanner3.nextInt();
+	    
+    	GuiMs game = new GuiMs(rows, columns, bombs);
+    }
+}
 
-
+	
